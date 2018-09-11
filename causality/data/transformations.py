c80e176fc36cad59988ee45f767ecc1d341b230e
MFreidank <freidankm@yahoo.de>
2018-09-11 16:35:16 +0200

    documentation for r_compatitibility decorator

--- causality/data/transformations.py
+++ causality/data/transformations.py
@@ -7,6 +7,29 @@ from functools import wraps
 #  R Compatibility {{{ #
 
 def to_robjects(covariates, observed_outcomes=None, treatment_assignment=None):
+    """ Transform the given data into `rpy2.robjects` objects that can be fed to `R`.
+        Returns transformed data as a dictionary.
+
+    Parameters
+    ----------
+    covariates : np.ndarray
+        Covariate data as a 2-d array of shape `(num_units, num_covariates)`.
+    observed_outcomes : np.ndarray, optional
+        Observed outcome data as a 1-d array of shape `(num_units,)`
+    treatment_assignment : np.ndarray, optional
+        (Binary) treatment assignment as a 1-d array of shape `(num_units,)`
+
+    Returns
+    ----------
+    robject_dictionary : dict
+        Dictionary mapping names of given data (e.g. `covariates`) to
+        corresponding `rpy2.robjects` objects.
+
+    Examples
+    ----------
+    TODO
+
+    """
     num_units, *_ = covariates.shape
 
     robjects = {
@@ -28,10 +51,32 @@ def to_robjects(covariates, observed_outcomes=None, treatment_assignment=None):
     return robjects
 
 
-def r_compatibility(function):
-    @wraps(function)
+def r_compatibility(method):
+    """ Decorates `method` allowing it to handle `numpy.ndarray` input as r-objects.
+        Used to provide an easy user interface to code that actually interfaces
+        to `R` code using `rpy2.robjects` objects inside the method body.
+
+    Parameters
+    ----------
+    method : TODO
+        Method to decorate. This method can be written as if all inputs are
+        `rpy2.robjects` objects, but once decorated all inputs can be passed
+        as `numpy.ndarray` objects instead.
+
+    Returns
+    ----------
+    decorated_method : TODO
+        Method that takes `numpy.ndarray` objects for its inputs and
+        uses `rpy2.robjects` inside its body to compute results.
+
+    Examples
+    ----------
+    TODO
+
+    """
+    @wraps(method)
     def wrapped(self, covariates, observed_outcomes=None, treatment_assignment=None, **kwargs):
-        return function(
+        return method(
             self,
             **to_robjects(
                 covariates=covariates,
