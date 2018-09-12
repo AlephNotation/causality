abf586c39ea4078c0455d081410de07ec20dfeaa
MFreidank <freidankm@yahoo.de>
2018-09-12 20:39:52 +0200

    added virtual twins approach

---
+++ causality/estimation/virtual_twins.py
@@ -0,0 +1,27 @@
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
+from causality.estimation.estimator import Estimator
+
+
+# TODO: There is an extension that includes additional information -> support that too
+# see: http://web.ccs.miami.edu/~hishwaran/papers/LSFI.JCGS2017.pdf
+class VirtualTwins(RandomForestRegressor, Estimator):
+    def fit(self, covariates, observed_outcomes, treatment_assignment, *args, **kwargs):
+        features = np.concatenate((covariates, np.expand_dims(treatment_assignment, axis=1)), axis=1)
+        print(features.shape)
+        super().fit(features, observed_outcomes)
+        return self
+
+    def predict(self, covariates):
+        num_covariates, *_ = covariates.shape
+        covariates_treated = np.concatenate(
+            (covariates, np.ones((num_covariates, 1))), axis=1
+        )
+
+        covariates_control = np.concatenate(
+            (covariates, np.zeros((num_covariates, 1))), axis=1
+        )
+        print(covariates.shape, covariates_control.shape)
+
+        return super().predict(covariates_treated) - super().predict(covariates_control)
--- causality/tests/test_ihdp.py
+++ causality/tests/test_ihdp.py
@@ -5,6 +5,7 @@ from causality.data.datasets.ihdp import IHDP
 from causality.estimation.causal_forest import CausalForest
 from causality.estimation.linear_regression import LinearRegression
 from causality.estimation.propensity_score_matching import NearestNeighborMatching
+from causality.estimation.virtual_twins import VirtualTwins
 from causality.diagnostics.synthetic_data import abs_ate
 
 
@@ -40,6 +41,19 @@ def test_nearest_neighbor_matching(replicate_number=0, n_neighbors=100):
         mu1=test_data.mu1,
         mu0=test_data.mu0,
         predicted_ate=NearestNeighborMatching(n_neighbors=n_neighbors).fit(
+            **train_data.asdict()
+        ).predict_ate(test_data.covariates)
+    )
+
+    assert np.allclose(error, 0., atol=0.1)
+
+
+def test_virtual_twins(replicate_number=0, n_neighbors=100):
+    train_data, test_data = IHDP.from_npz(replicate_number=replicate_number)
+    error = abs_ate(
+        mu1=test_data.mu1,
+        mu0=test_data.mu0,
+        predicted_ate=VirtualTwins().fit(
             **train_data.asdict()
         ).predict_ate(test_data.covariates)
     )
