4201af94b7c0bbda49f362f461e78f59b0dfe069
MFreidank <freidankm@yahoo.de>
2018-10-17 15:29:16 +0200

    pep508 url?

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         packages=find_packages(),
         package_data={"docs": ["*"]},
         include_package_data=True,
-        install_requires=INSTALL_REQUIREMENTS + ["https://github.com/MFreidank/cfrnet.git#egg=cfrnet"],
+        install_requires=INSTALL_REQUIREMENTS + ["pip @ https://github.com/MFreidank/cfrnet.git#egg=cfrnet"],
         setup_requires=SETUP_REQUIREMENTS,
         tests_require=TEST_REQUIREMENTS,
     )
